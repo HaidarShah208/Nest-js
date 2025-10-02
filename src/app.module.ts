7d91366b06846c61712f698bf3e76483505a9eac
Muhammad Zaid <ahmed.mehmood5036@gmail.com>
2025-10-02 12:54:23 +0500

    Implement LoggerMiddleware and integrate it into AppModule

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,13 +1,18 @@
-import { Module } from '@nestjs/common';
+import { MiddlewareConsumer, Module, NestModule } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { UsersModule } from './users/users.module';
 import { UserRolesController } from './user-roles/user-roles.controller';
 import { UsersRolesModule } from './user-roles/user-roles.module';
+import { LoggerMiddleware } from './middleware/logger/logger.middleware';
 
 @Module({
   imports: [UsersModule,UsersRolesModule],
   controllers: [AppController, UserRolesController,UserRolesController],
   providers: [AppService],
 })
-export class AppModule {}
+export class AppModule implements NestModule {
+configure(consumer: MiddlewareConsumer) {
+    consumer.apply(LoggerMiddleware).forRoutes('*');
+}
+}
